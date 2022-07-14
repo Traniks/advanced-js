a8cc2e538278f0f37d394c0787e200f058a76143
Traniks <andert2017@gmail.com>
2022-07-14 19:21:19 +0300

    107

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// 107. (д) Создание своих ошибок
+"use strict";
+
+const data = [
+    {
+        id: "box",
+        tag: "div"
+    },
+    {
+        id: "fff",
+        tag: "nav"
+    },
+    {
+        id: "circle",
+        tag: ""
+    }
+];
+
+try {
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+    
+        if (!blockObj.id) throw new SyntaxError(`В данных под номером ${i + 1} нет id`);
+    
+        block.setAttribute("id", blockObj.id);
+        document.body.append(block);
+    });
+} catch(e) {
+    if (e.name === "SyntaxError") {
+        console.log(e.message);
+    } else throw e;
+}
+
+// const err = new SyntaxError("1");
+// console.log(err.name, err.message, err.stack);
+
 //106106106106106106106106106106. Ошибки. Как избежать “поломки” своего кода
 // "use strict";
 
