5a87e0e30920a722b676e42639967a0d22a8a4ff
Traniks <andert2017@gmail.com>
2022-07-16 13:11:45 +0300

    110

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-import "jquery";
+import $ from "jquery";
+
+$(document).ready(function() {
+    $(".list-item:first").hover(function() {
+        $(this).toggleClass("active");
+    });
+
+    $(".list-item:eq(2)").on("click", function() {
+        $(".image:even").fadeToggle("slow");
+    });
+
+    $(".list-item:eq(4)").on("click", function() {
+        $(".image:odd").animate({
+            onpaste: "toggle",
+            height: "toggle"
+        }, 2000);
+    });
+});
